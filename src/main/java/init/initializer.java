b5dadc4f8396ac3f77e18c33b29f48fa8b5cc3e9
Gaurav <gaurav maheshwari>
2016-11-04 23:39:05 +0100

    V0.0.1.a - Intermediate commit. Almost done with settign up word annotation. TODO: Add token annotation TODO: change package name.

--- src/main/java/init/initializer.java
+++ src/main/java/init/initializer.java
@@ -1,28 +1,6 @@
 package init;
-//TODO: import correction
-//import edu.stanford.nlp.*;
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
-import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.util.PropertiesUtils;
-//import edu.stanford.nlp.*;
-import edu.stanford.nlp.simple.*;
-
-import java.util.*;
-
 import nlp.nlpInit;
+
 public class initializer {
 //	This class initializes all the library needed by asknow which involves 
 //	stanfordnlp
---
+++ src/main/java/tokenAnnotation/tokenAnnotationOrch.java
@@ -0,0 +1,15 @@
+package tokenAnnotation;
+
+import java.util.ArrayList;
+
+public class tokenAnnotationOrch {
+/*
+ * This class acts as a Orchestrator for annotation of word and making a array of word object. 
+ * 
+ * */
+	
+	public void getWordTokenArrayList(String question){
+		//returns arraylist of annotated word tokens object. 
+		
+	}
+}
