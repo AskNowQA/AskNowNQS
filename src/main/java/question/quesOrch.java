090352c7f1def9af5abacdf57056778d01fb4d39
Gaurav <gaurav maheshwari>
2016-11-10 11:37:36 +0100

    V 0.0.3 - Token Annotation works and get's stored in sentence object. Next step is token merger rules and storage.

--- src/main/java/question/quesOrch.java
+++ src/main/java/question/quesOrch.java
@@ -1,6 +1,10 @@
 package question;
 
+import java.util.ArrayList;
+
 import preProcessing.preProcessing;
+import tokenAnnotation.token;
+import tokenAnnotation.tokenAnnotationOrch;
 
 public class quesOrch {
 	
@@ -13,10 +17,16 @@ public class quesOrch {
 	{
 		//TODO:Discuss, if it needs to be moved to another class
 		
+		//question preprocessing
 		questionAnnotation ques = new questionAnnotation(question);
 		preProcessing pre = new preProcessing();
 		String processedQuestion = pre.preProcessingOrchestrator(question);
 		ques.setpreProcessingQuestion(processedQuestion);
+		
+		//word based annotation
+		tokenAnnotationOrch tokenAnnotation = new tokenAnnotationOrch();
+		ArrayList<token> tokenList = tokenAnnotation.getWordTokenArrayList(processedQuestion);
+		ques.settokenlist(tokenList);
 		return processedQuestion;
 		
 		
