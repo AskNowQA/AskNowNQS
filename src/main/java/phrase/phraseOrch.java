c7e7a55fde0763c663a7b96511fc781be0e57165
Gaurav <gaurav maheshwari>
2016-11-14 17:02:21 +0100

    V 0.0.4.b - Added ner merger.

--- src/main/java/phrase/phraseOrch.java
+++ src/main/java/phrase/phraseOrch.java
@@ -3,6 +3,7 @@ package phrase;
 
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
@@ -23,44 +24,74 @@ public class phraseOrch {
 	
 	public void phraseMergerOrchestrator(questionAnnotation questionAnnotation){
 		
-		//pass it through DbPedia spotlight
-		ArrayList<String> labelList = spotlightAnnotation(questionAnnotation.getpreProcessingQuestion());
+//		spotLightMerger(questionAnnotation);
+		nerMerger(questionAnnotation);
+		System.out.println("emm some one there ?");
+		printMetaPhrase();
+	}
+	
+	public void nerMerger(questionAnnotation questionAnnotation) {
+		Pattern nerTags = Pattern.compile("LOCATION|PERSON|ORGANIZATION|MISC");
+
 		
 		
-		ArrayList<token> tokenlist = questionAnnotation.gettokenlist();
-		
-		
-		
-		System.out.println(questionAnnotation.gettokenlist());
-		for (String label : labelList){
-//			//splitting token by white space
-//			String[] splited = label.split("\\s+");
-			System.out.println("label is " + label);
+		for (int i=0 ; i< questionAnnotation.gettokenlist().size();i++){
+			System.out.println(questionAnnotation.gettokenlist().get(i).getNerTag());
 			
-			
-			ArrayList<token> phrase_token = new ArrayList<token>();
-			
-			phrase ph = new phrase();
-			
-			for (token word : tokenlist){
-				if (label.contains(word.getValue())){
-					phrase_token.add(word);
-					word.setIsPartOfPhrase(true);
+			if(nerTags.matcher(questionAnnotation.gettokenlist().get(i).getNerTag()).find() && !questionAnnotation.gettokenlist().get(i).isPartOfPhrase()){
+				ArrayList<token> phrase_token = new ArrayList<token>();
+				phrase_token.add(questionAnnotation.gettokenlist().get(i));
+				questionAnnotation.gettokenlist().get(i).setIsPartOfPhrase(true);
+				while(questionAnnotation.gettokenlist().get(i+1).getNerTag() == questionAnnotation.gettokenlist().get(i).getNerTag()){
+					if (questionAnnotation.gettokenlist().get(i+1).isPartOfPhrase() == false){
+						phrase_token.add(questionAnnotation.gettokenlist().get(i+1));
+						questionAnnotation.gettokenlist().get(i+1).setIsPartOfPhrase(true);
+						i = i + 1;
+					}
 				}
-			}
-			ph.setPhraseToken(phrase_token);
-			
-			ph.setPosTag("SP"); //spotlight token annotation --> SP
-			metaPhrase.add(ph);
-		}
-		
-		for(phrase ph : metaPhrase){
-			System.out.println("phrase are:");
-			for (token tk : ph.getPhraseToken()){
-				System.out.println(tk.getValue());
+				phrase ph = new phrase();
+				ph.setPhraseToken(phrase_token);
+				ph.setPosTag("NER");
+				metaPhrase.add(ph);
 			}
 		}
 	}
+	
+	
+	public void spotLightMerger(questionAnnotation questionAnnotation){
+		//pass it through DbPedia spotlight
+				ArrayList<String> labelList = spotlightAnnotation(questionAnnotation.getpreProcessingQuestion());
+				
+				
+				ArrayList<token> tokenlist = questionAnnotation.gettokenlist();
+				
+				
+				
+				System.out.println(questionAnnotation.gettokenlist());
+				for (String label : labelList){
+//					//splitting token by white space
+//					String[] splited = label.split("\\s+");
+					System.out.println("label is " + label);
+					
+					
+					ArrayList<token> phrase_token = new ArrayList<token>();
+					
+					phrase ph = new phrase();
+					
+					for (token word : tokenlist){
+						if (label.contains(word.getValue())){
+							phrase_token.add(word);
+							word.setIsPartOfPhrase(true);
+						}
+					}
+					ph.setPhraseToken(phrase_token);
+					
+					ph.setPosTag("SP"); //spotlight token annotation --> SP
+					metaPhrase.add(ph);
+				}
+	}
+	
+	
 	
 	public ArrayList<String> spotlightAnnotation(String question)
 	{
@@ -82,5 +113,14 @@ public class phraseOrch {
 			e.printStackTrace();
 		}
 		return label;
+	}
+	
+	public void printMetaPhrase(){
+		for(phrase ph : metaPhrase){
+			System.out.println("phrase are:");
+			for (token tk : ph.getPhraseToken()){
+				System.out.println(tk.getValue());
+			}
+		}
 	}
 }
