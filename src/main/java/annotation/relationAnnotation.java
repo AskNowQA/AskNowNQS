b1c3b4bc67c8526adbbc3d0091fdbfc7e321ca04
Gaurav <saist1993@gmail.com>
2016-12-04 02:57:59 +0100

    V 0.0.6.d - Intermediate commit

--- src/main/java/annotation/relationAnnotation.java
+++ src/main/java/annotation/relationAnnotation.java
@@ -1,6 +1,7 @@
 package annotation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -18,6 +19,7 @@ public class relationAnnotation {
 	
 	public static void relAnnotation(ArrayList<phrase> phraseList, questionAnnotation ques_annotation) throws Exception{
 		System.out.println("at relation annotation");
+		String[] stopWord = {"Who","What","Who"};
 		for (phrase ph : phraseList){
 			if (ph.getUri() != null){
 				//Retrives list of relation coming in and going out of the annotated entity 
@@ -40,9 +42,25 @@ public class relationAnnotation {
 				
 				ArrayList<String[]> listOfPairScore = new ArrayList<String[]>();
 				
+//				for (String[] lp : listOfPair){
+//					for ( token tk : ques_annotation.gettokenlist())
+//					listOfPairScore.add(new String [] {lp[0],lp[1],tk.getValue(),String.valueOf(word2vec.sendToVec(lp[1], tk.getValue()))});
+//				}
+				
+				
+//TODO: Simplify this for loop 				
 				for (String[] lp : listOfPair){
-					for ( token tk : ques_annotation.gettokenlist())
-					listOfPairScore.add(new String [] {lp[0],lp[1],tk.getValue(),String.valueOf(word2vec.sendToVec(lp[1], tk.getValue()))});
+					for (phrase phr : phraseList){
+						if (phr.getUri() == null ){
+							for (token tk : phr.getPhraseToken()){
+								if (!Arrays.asList(stopWord).contains(tk.getValue())) {
+									String score = String.valueOf(word2vec.sendToVec(lp[1], tk.getValue()));
+									if (Float.valueOf(score) > -1.0){		
+									listOfPairScore.add(new String [] {lp[0],lp[1],tk.getValue(),score});}
+								}
+							}
+						}
+					}
 				}
 				
 						
@@ -55,8 +73,7 @@ public class relationAnnotation {
 				
 				for(String[] lp : listOfPairScore){
 					System.out.println( lp[1]+ " :: " + lp[2] + " :: " + lp[3]);
-				}
-				
+				}				
 			}
 		}
 	}
